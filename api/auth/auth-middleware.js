43093eaac9964a23b74ad36d8e708419878a6d3a
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-04-20 14:48:44 -0400

    finished auth register func

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,6 +1,20 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken');
 
 const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(401).json({message: 'Token required'})
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).json({message: 'Token invalid'});
+      } else {
+        req.decodedToken = decoded;
+        next();
+      }
+    });
+  }
   /*
     If the user does not provide a token in the Authorization header:
     status 401
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const Users = require('../users/users-model');
+const bcrypt = require('bcryptjs');
 
 router.post("/register", validateRoleName, (req, res, next) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+  .then(newUser => {
+    res.status(201).json(newUser);
+  })
+  .catch(next);
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
